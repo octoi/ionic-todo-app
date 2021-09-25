3f18e26828e852d56d9cae19698ad505f5001f41
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-25 11:06:17 +0530

    local storage implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   IonContent,
   IonHeader,
@@ -25,6 +25,34 @@ interface Todo {
 const Home: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  useEffect(() => {
+    if (!localStorage) return;
+
+    let savedTodos = localStorage.getItem('todos');
+    if (savedTodos && savedTodos.length !== 0) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  const addTodo = (v: string) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Date.now().toString(),
+        value: v,
+        checked: false,
+      },
+    ];
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
+
+  const deleteTodo = (id: string) => {
+    const newTodos = todos.filter((todoItem) => todoItem.id !== id);
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -34,18 +62,7 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <div className='p'>
-          <TodoInput
-            addTodo={(v) =>
-              setTodos([
-                ...todos,
-                {
-                  id: Date.now().toString(),
-                  value: v,
-                  checked: false,
-                },
-              ])
-            }
-          />
+          <TodoInput addTodo={addTodo} />
           {todos.length !== 0 && (
             <IonList style={{ marginTop: '20px', padding: '15px' }}>
               {todos?.map((todo) => (
@@ -65,18 +82,14 @@ const Home: React.FC = () => {
                           return todoItem;
                         });
                         setTodos(editedTodos);
+                        localStorage.setItem(
+                          'todos',
+                          JSON.stringify(editedTodos)
+                        );
                       }}
                     />
                   </IonItem>
-                  <IonButton
-                    onClick={() => {
-                      const newTodos = todos.filter(
-                        (todoItem) => todoItem.id !== todo.id
-                      );
-                      setTodos(newTodos);
-                    }}
-                    color='danger'
-                  >
+                  <IonButton onClick={() => deleteTodo(todo.id)} color='danger'>
                     <IonIcon icon={trash} />
                   </IonButton>
                 </div>
