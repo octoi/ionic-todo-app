2647911a90860b9a8a1cd700590c9c88f73058ff
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-25 10:41:02 +0530

    form submit

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,17 +8,25 @@ interface Props {
 export default function TodoInput({ addTodo }: Props) {
   const [value, setValue] = useState('');
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+
+    setValue('');
+    addTodo(value);
+  };
+
   return (
-    <div className='todo-input-container'>
+    <form onSubmit={handleSubmit} className='todo-input-container'>
       <IonInput
         placeholder='Enter new todo'
         value={value}
         onIonChange={(e: any) => setValue(e.target?.value)}
         className='todo-input'
       />
-      <IonButton size='large' disabled={!value} onClick={() => addTodo(value)}>
+      <IonButton size='large' disabled={!value} type='submit'>
         Add Todo
       </IonButton>
-    </div>
+    </form>
   );
 }
